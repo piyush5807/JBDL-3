8f2535525bfe11def2c53c20e25bd38d84e68056
Piyush Aggarwal <piyush5807@gmail.com>
2020-04-05 23:46:56 +0530

    added JDBC code

---
+++ JDBC/src/main/java/com/example/jdbcdemoex/demoexampledbc/DemoExampleDbcApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbcdemoex.demoexampledbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoExampleDbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoExampleDbcApplication.class, args);
+	}
+
+}
---
+++ JDBC/src/main/java/com/example/jdbcdemoex/demoexampledbc/Person.java
@@ -0,0 +1,56 @@
+package com.example.jdbcdemoex.demoexampledbc;
+
+public class Person {
+
+    public int id;
+    private String name;
+    private String surname;
+    private int age;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String toString(){
+        return "id = " + this.id + "name = " + this.getName() + ", surname = " + this.getSurname() + ", age = " + this.getAge();
+    }
+
+    public Person(int id, String name, String surname, int age) {
+        this.id = id;
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
+    }
+
+    public Person() {
+    }
+}
