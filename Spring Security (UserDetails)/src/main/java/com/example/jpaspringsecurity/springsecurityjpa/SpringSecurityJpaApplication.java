3b15990b4984ec18e2d1a47eed110dafcad2f330
Piyush Aggarwal <piyush5807@gmail.com>
2020-04-25 23:36:05 +0530

    added spring security using db

---
+++ Spring Security (UserDetails)/src/main/java/com/example/jpaspringsecurity/springsecurityjpa/SpringSecurityJpaApplication.java
@@ -0,0 +1,26 @@
+package com.example.jpaspringsecurity.springsecurityjpa;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJpaApplication implements CommandLineRunner {
+
+	@Autowired
+	UserServiceRepository userServiceRepository;
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJpaApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		userServiceRepository.save(new MyUserDetails("piyush", "pass", true,
+				"USER:ADMIN"));
+		userServiceRepository.save(new MyUserDetails("abc", "1234", true,
+				"USER"));
+	}
+}
