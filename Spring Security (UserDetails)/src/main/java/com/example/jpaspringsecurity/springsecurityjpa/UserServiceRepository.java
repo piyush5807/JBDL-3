8513824054e7f698f4a8f0c2f6733d22c94c21bb
Piyush Aggarwal <piyush5807@gmail.com>
2020-04-26 19:58:43 +0530

    modified jwt code

--- Spring Security (UserDetails)/src/main/java/com/example/jpaspringsecurity/springsecurityjpa/UserServiceRepository.java
+++ Spring Security (UserDetails)/src/main/java/com/example/jpaspringsecurity/springsecurityjpa/UserServiceRepository.java
@@ -3,6 +3,8 @@ package com.example.jpaspringsecurity.springsecurityjpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.User;
 
+import java.util.List;
+
 public interface UserServiceRepository extends JpaRepository<MyUserDetails, Integer> {
 
     MyUserDetails findByUserName(String username);
